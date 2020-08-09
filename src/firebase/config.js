42485bc553a0c9369da1c4dcaeb0757031bd1940
Rohan Pareek <rohaniscoding@gmail.com>
2020-08-09 20:53:37 +0530

    testing env vars

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,12 +17,12 @@ var firebaseConfig = {
   console.log('firebaseConfig', firebaseConfig)
 
   var firebaseConfig2 = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    apiKey: process.env.REACT_API_KEY,
+    authDomain: process.env.REACT_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_DATABASE_URL,
+    projectId: process.env.REACT_PROJECT_ID,
+    storageBucket: process.env.REACT_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
     appId: process.env.APP_ID
   };
 
