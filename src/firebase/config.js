ffe9c4ee78d0c2c1893e09c078b529469033c473
Rohan Pareek <rohaniscoding@gmail.com>
2020-08-09 17:46:25 +0530

    changes config keys setting

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+import config from '../.env';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    databaseURL: config.databaseURL,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const storage = firebase.storage();
+  const fireStore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export { storage, fireStore, timestamp };
